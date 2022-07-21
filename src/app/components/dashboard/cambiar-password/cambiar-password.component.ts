b0c305fd5f706bb1fc2b39263b1d6e746aa59f55
Gerardo Alvarez Mendoza <gerardam@outlook.com>
2022-07-21 12:23:50 -0500

    Maquetado y validaciones de cambiar contraseña

--- src/app/components/dashboard/cambiar-password/cambiar-password.component.ts
+++ src/app/components/dashboard/cambiar-password/cambiar-password.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cambiar-password',
   templateUrl: './cambiar-password.component.html',
-  styleUrls: ['./cambiar-password.component.css']
+  styleUrls: ['./cambiar-password.component.css'],
 })
 export class CambiarPasswordComponent implements OnInit {
+  cambiarPassword: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.cambiarPassword = this.fb.group(
+      {
+        passwordAnterior: ['', Validators.required],
+        nuevoPassword: ['', [Validators.required, Validators.minLength(4)]],
+        confirmPassword: [''],
+      },
+      { validator: this.checkPassword }
+    );
   }
 
+  ngOnInit(): void {}
+
+  guardarPassword(): void {
+    console.log(this.cambiarPassword);
+  }
+
+  checkPassword(group: FormGroup): any {
+    const pass = group.controls['nuevoPassword'].value;
+    const confirmPass = group.controls['confirmPassword'].value;
+    return pass === confirmPass ? null : { notSame: true };
+  }
 }
