aaae4110fc8d31f1bfae7b953de78a98548bfdf1
Gerardo Alvarez Mendoza <gerardam@outlook.com>
2022-07-29 11:20:26 -0500

    Creacion de user service

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,12 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 import { Usuario } from '../models/usuario';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UsuarioService {
-  constructor(private http: HttpClient) {}
+  myAppUrl: string;
+  myApiUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.myAppUrl = environment.endpoint;
+    this.myApiUrl = '/api/usuario';
+  }
 
   saveUser(usuario: Usuario): void {}
 }
